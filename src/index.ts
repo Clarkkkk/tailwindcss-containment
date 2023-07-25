b100b05f2e11580ec7bf7849f36539ac26a3d02a
Aaron_Zhou <clark1729@outlook.com>
2023-07-25 18:21:34 +0800

    feat: plugin including `contain`, `content-visibility`, `contain-intrinsic-size` utilities

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-import './style.css'
+import createPlugin from 'tailwindcss/plugin'
+import type { Config, PluginCreator } from 'tailwindcss/types/config'
+import { intrinsicMatchUtilities, intrinsicUtilities } from './intrinsic'
 
-export { sum } from './sum'
+const plugin: {
+    handler: PluginCreator
+    config?: Partial<Config> | undefined
+} = createPlugin(function ({ addUtilities, matchUtilities }) {
+    addUtilities([
+        {
+            '.contain-none': { contain: 'none' },
+            '.contain-strict': { contain: 'strict' },
+            '.contain-content': { contain: 'content' },
+            '.contain-size': { contain: 'size' },
+            '.contain-inline-size': { contain: 'inline-size' },
+            '.contain-layout': { contain: 'layout' },
+            '.contain-style': { contain: 'style' },
+            '.contain-paint': { contain: 'paint' }
+        },
+        {
+            '.content-auto': {
+                'content-visibility': 'auto'
+            },
+            '.content-hidden': {
+                'content-visibility': 'hidden'
+            },
+            '.content-visible': {
+                'content-visibility': 'visible'
+            }
+        },
+        ...intrinsicUtilities
+    ])
+
+    matchUtilities({
+        contain: (value) => ({
+            contain: value
+        }),
+        ...intrinsicMatchUtilities
+    })
+})
+
+export default plugin
